93aa9cd143554fe6fa9785c2c218f1cf640463b4
Omkar Kathi <omkarinbrn@gmail.com>
2024-12-09 11:25:20 +0530

    Quartz sync: Dec 9, 2024, 11:25 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "Omkar211202.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian","excalidraw"],
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
